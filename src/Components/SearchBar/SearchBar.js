746b87750a5a8983e12332e7f1d28b2879704945
Julian Fauricio Valencia Marin <jvalencia@reefstudios.co>
2019-08-15 01:06:00 -0500

    add test

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Input } from 'antd';
 import classes from './SearchBar.module.scss';
 const { Search } = Input;
-export function SearchBar({ value, setValue }) {
+export function SearchBar({ setValue }) {
   return (
     <div className={classes.root}>
       <Search
         placeholder="Buscar repositorio"
-        // value={value}
         onChange={e => setValue(e.target.value)}
       />
     </div>
---
+++ src/Services/githubServices.test.js
@@ -0,0 +1,12 @@
+import GithubService from './github.services';
+import { doesNotReject } from 'assert';
+const githubService = new GithubService();
+
+describe('Traer todos los repositorios', () => {
+  test('Haciendo una peticion a la api de github', done => {
+    githubService.getReposUser('FauricioValencia').then(data => {
+      expect(data.length).toBeGreaterThan(0);
+      done();
+    });
+  });
+});
